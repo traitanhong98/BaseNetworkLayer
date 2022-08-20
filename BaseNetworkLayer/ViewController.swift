b430451d4925a819111966d2a9a534c706f6c4f5
An <annx2812@gmail.com>
2022-08-20 10:11:25 +0700

    Initial Commit

---
+++ BaseNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseNetworkLayer
+//
+//  Created by Nguyễn Hoàng on 20/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
